a8d530334d844f1e477a36c7c45582e610a9a7b0
tofe.al2mora@gmail.com <La311999>
2020-02-24 23:35:11 +0300

    welcom

---
+++ JS/welcomPage.js
@@ -0,0 +1,58 @@
+
+
+function openForm(clicked) {
+    if(clicked=="signUp"){
+      document.getElementById("signup").style.display = "block";
+      document.getElementById("login").style.display = "none";
+      document.getElementById("forgetPassword").style.display = "none";
+
+    }else if(clicked=="login"){
+      document.getElementById("login").style.display = "block";
+      document.getElementById("signup").style.display = "none";
+      document.getElementById("forgetPassword").style.display = "none";
+
+  
+    }
+    
+    }
+
+    
+    function swichForm(clicked) {
+      if(clicked=="signUp"){
+        document.getElementById("signup").style.display = "block";
+        document.getElementById("login").style.display = "none";
+        document.getElementById("forgetPassword").style.display = "none";
+
+  
+      }else if(clicked=="login"){
+        document.getElementById("login").style.display = "block";
+        document.getElementById("signup").style.display = "none";
+        document.getElementById("forgetPassword").style.display = "none";
+
+  
+    
+      }else if(clicked == "forgetPass"){
+        document.getElementById("forgetPassword").style.display = "block";
+        document.getElementById("login").style.display = "none";
+      }
+      
+      }
+    
+    
+    function scrollFunction() {
+      mybutton = document.getElementById("myBtn");
+  
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }
+    }
+    
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
+    window.addEventListener("scroll",scrollFunction,false);
+  
