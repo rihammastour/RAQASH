59b5b2c6632717070b9e7ac28c5532393530f5c8
tofe.al2mora@gmail.com <La311999>
2020-02-24 23:33:59 +0300

    js fils

--- JS/viewArtwork.js
+++ JS/viewArtwork.js
@@ -2,5 +2,7 @@ function start(){
 
     var currentDate = new Date();
     document.getElementById("date").innerHTML=currentDate.getDate()+"-"+currentDate.getMonth()+"-"+currentDate.getFullYear();
+ 
 }
+
 window.addEventListener("load",start,false);
