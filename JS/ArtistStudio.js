2fbaa9c0044677787dd79a8aff2f2eee950db310
mihaf <mihafsalman11@hotmail.com>
2020-03-27 18:06:57 +0300

    add

--- JS/ArtistStudio.js
+++ JS/ArtistStudio.js
@@ -47,7 +47,8 @@ function start(){
            
           });
           $(".close").click(function(){
-            $(".manage-artwork","#bg-model").hide();
+            $(".manage-artwork","#bg-model",).hide();
+
           
            
           });
