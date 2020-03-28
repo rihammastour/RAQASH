5810e7e3577dd4dd65d7fd2669e55689275cbaf6
tofe.al2mora@gmail.com <La311999>
2020-03-28 15:34:45 +0300

    fill buttons

--- JS/onClickButtons.js
+++ JS/onClickButtons.js
@@ -1,11 +1,23 @@
 
 $(document).ready(function () {
-         $("#viewArt").hide();
+        $("#viewArt").hide();
 
+        //open art work
+        $(".post").click(function () {
 
-         $(".post").click(function () {
                 $("#commentsPost").hide();
 
+
+                //get attributr of like , dislike , fav 
+                var like = $(this).find(".like").attr("src");
+                var dislike = $(this).find(".dislike").attr("src");
+                var fav = $(this).find(".favbutton").attr("src");
+
+                $("#viewArt").find(".like").attr("src", like);
+                $("#viewArt").find(".dislike").attr("src", dislike);
+                $("#viewArt").find(".favbutton").attr("src", fav);
+
+                // get art img
                 var attr = $(this).attr('src');
 
                 // For some browsers, `attr` is undefined; for others, `attr` is false. Check for both.
@@ -20,29 +32,13 @@ $(document).ready(function () {
                         $("#userPost").attr("src", bg);
                 }
 
-
-
-                var like = $(this).find(".like").attr("src");
-                var dislike = $(this).find(".dislike").attr("src");
-                var fav = $(this).find(".favbutton").attr("src");
-
-                if (like == "assets/Raqash Icon-filled like.png") {
-                        $("#viewArt").find(".like").attr("src", "assets/Raqash Icon-filled like.png");
-                }
-                if(dislike == "assets/Raqash Icon-filled dislike.png"){
-                        $("#viewArt").find(".dislike").attr("src", "assets/Raqash Icon-filled dislike.png");
-                }
-                if(fav == "assets/Raqash Icon-favActivated.svg"){
-                        $("#viewArt").find("#favbutton").attr("src", "assets/Raqash Icon-favActivated.svg");
-                }
-
+                
+               //position of the view artwork  in ghe document
                 var pos = $(this).css("padding");
                 var width = $(this).outerWidth();
 
-
-
                 $("#viewArt ").css("position", "absolute");
-                $("#viewArt ").css("top", pos + "px");
+                $("#viewArt ").css("top", pos.top + "px");
                 $("#viewArt ").css("left", 25 + "%");
 
                 $("#commentsPost").css("position", "absolute");
@@ -53,41 +49,39 @@ $(document).ready(function () {
 
 
         });
-
+    
+        //close button
         $(".close").click(function () {
+
                 $("#viewArt").hide();
                 $("#bg-model").hide();
                 $("#commentsPost").hide();
-          
-
-
 
         });
 
         //close the div when click any where out side the div
         $(document).mouseup(function (e) {
+
                 var container = $("#viewArt");
                 var container2 = $("#commentsPost");
- 
 
                 // if the target of the click isn't the container nor a descendant of the container
-                if (!container.is(e.target) && container.has(e.target).length === 0 && container2.has(e.target).length === 0&&!container2.is(e.target)) {
-                        if($("#viewArt").is(":visible")){
-                        container.hide();
-                        $("#bg-model").hide();
-                        $("#commentsPost").hide();
+                if (!container.is(e.target) && container.has(e.target).length === 0 && container2.has(e.target).length === 0 && !container2.is(e.target)) {
+                        if ($("#viewArt").is(":visible")) {
+                                container.hide();
+                                $("#bg-model").hide();
+                                $("#commentsPost").hide();
                         }
 
                 }
 
         });
-
+        
+        //fill buttons
         $(".like").click(function () {
+
                 $(this).attr("src", "assets/Raqash Icon-filled like.png");
                 $(this).siblings(".dislike").attr("src", "assets/Raqash Icon-dislike.svg");
-
-                // $(".dislike").attr("src","assets/Raqash Icon-dislike.svg")
-
 
         });
 
@@ -99,11 +93,12 @@ $(document).ready(function () {
 
         });
 
-        $(".favbutton").click(function(){
-                $(this).attr("src" ,"assets/Raqash Icon-favActivated.svg" );
-        
-        });
+        $(".favbutton").click(function () {
+                $(this).attr("src", "assets/Raqash Icon-favActivated.svg");
 
+        });
+          
+        //in home pages disable the img info div so it does not show view art onclick
         $(".disable").click(function () {
                 return false;
         });
@@ -111,25 +106,27 @@ $(document).ready(function () {
 
 
 });
+  
+
 $(document).ready(function () {
         $('[data-toggle="popover"]').popover();
-      });
+});
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                mybutton.style.display = "block";
+        } else {
+                mybutton.style.display = "none";
+        }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
